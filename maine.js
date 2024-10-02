e92569594b5bdf54996d1f0ebd8f781c88956a30
SayedBela <sayed.belal@myenaip.eu>
2024-10-02 07:34:42 +0000

    Belal

---
+++ maine.js
@@ -0,0 +1,17 @@
+let posX = 0;
+let posY = 0;
+let diametro =20;
+     function setup(){
+        createCanvas(600,600);
+        background(120,255,50);
+      }
+
+function draw(){
+    noStroke();
+
+    CSSNumericValue(posX+10,posy+10,diametro);
+
+    posX= posX + 10;
+    posY= posY + 10;
+    
+}
